5fa595137c6870ebcb520a19922ab31f41ef5323
Akhilesh Mishra <akhileshmishra13april91@gmail.com>
2026-01-09 23:01:53 +0800

    fix: use AES256 encryption for training bucket to allow presigned URL access

--- terraform/modules/storage/main.tf
+++ terraform/modules/storage/main.tf
@@ -27,8 +27,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "training_data" {
 
   rule {
     apply_server_side_encryption_by_default {
-      kms_master_key_id = var.kms_key_id
-      sse_algorithm     = "aws:kms"
+      sse_algorithm = "AES256"
     }
   }
 }
